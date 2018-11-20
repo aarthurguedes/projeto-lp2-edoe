d33e9f443690517f5dc67830df01a53ce5dd7dbd
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2018-11-20 19:22:34 -0300

    Alteração na documentação do código

--- eDoe/src/util/util.java
+++ eDoe/src/util/util.java
@@ -4,13 +4,14 @@ package util;
  * Classe criada para armazenar utilidades do codigo.
  *
  * @author Antonio Bertino de Vasconcelos Cabral Neto
- * @author Arthur Silva Guedes
+ * @author Arthur Silva Lima Guedes
  * @author Danilo de Menezes Freitas
  * @author Talita Galdino Gouveia
  */
 public class util {
     /**
-     * Metodo criado para formatar uma String para um padrao unico. No caso, Em letras minusculas e sem espacos no inicio e no fim.
+     * Metodo criado para formatar uma String para um padrao unico. No caso, Em letras minusculas e sem espacos no inicio 
+     * e no fim.
      *
      * @param msg String com mensagem a ser formatada.
      * @return String com mensagem formatada no padrao previante dito.
